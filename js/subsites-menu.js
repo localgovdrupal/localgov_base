3d692723fbe27aadddd2d4ed764d0c736a196ca0
Mark Conroy <mark@mark.ie>
2024-05-19 16:11:53 +0100

    formatting the JS

--- js/subsites-menu.js
+++ js/subsites-menu.js
@@ -5,28 +5,34 @@
 (function subsitesMenuScript(Drupal) {
   Drupal.behaviors.subsitesMenu = {
     attach: function (context) {
-
       // This variable will be used later to make sure that the window
       // was actually resized.
       let windowWidth = window.innerWidth;
 
-      const subsitesMenuToggle = document.querySelector('.subsite-extra__header-toggle-button');
-      const subsitesMenu = document.querySelector('.subsite-extra-menu');
+      const subsitesMenuToggle = document.querySelector(
+        ".subsite-extra__header-toggle-button"
+      );
+      const subsitesMenu = document.querySelector(".subsite-extra-menu");
 
-      subsitesMenuToggle.addEventListener('click', function () {
-        subsitesMenuToggle.setAttribute('aria-expanded', subsitesMenuToggle.getAttribute('aria-expanded') === 'true' ? 'false' : 'true');
-        subsitesMenu.classList.toggle('subsite-extra-menu--active');
+      subsitesMenuToggle.addEventListener("click", function () {
+        subsitesMenuToggle.setAttribute(
+          "aria-expanded",
+          subsitesMenuToggle.getAttribute("aria-expanded") === "true"
+            ? "false"
+            : "true"
+        );
+        subsitesMenu.classList.toggle("subsite-extra-menu--active");
       });
 
       function handleReset() {
-        subsitesMenuToggle.setAttribute('aria-expanded', 'false');
-        subsitesMenu.classList.remove('subsite-extra-menu--active');
+        subsitesMenuToggle.setAttribute("aria-expanded", "false");
+        subsitesMenu.classList.remove("subsite-extra-menu--active");
       }
 
       // If the window is resized to more than 768px, reset the menu.
       function handleWindowResized() {
         if (window.innerWidth === windowWidth) {
-          return
+          return;
         } else {
           windowWidth = window.innerWidth;
         }
@@ -36,23 +42,25 @@
       }
 
       // Close the menu when the escape key is pressed.
-      context.addEventListener('keydown', function(e) {
-        if (e.key == 'Escape') {
+      context.addEventListener("keydown", function (e) {
+        if (e.key == "Escape") {
           e.preventDefault();
-          handleReset()
+          handleReset();
           subsitesMenuToggle.focus();
         }
       });
 
       // Close the menu when a click is made outside of it.
-      document.addEventListener('click', function(e) {
-        if (!e.target.closest('#lgd-header__nav--subsites-menu')){
+      document.addEventListener("click", function (e) {
+        if (!e.target.closest("#lgd-header__nav--subsites-menu")) {
           handleReset();
         }
       });
 
-      window.addEventListener('resize', Drupal.debounce(handleWindowResized, 50, false));
-
-    }
+      window.addEventListener(
+        "resize",
+        Drupal.debounce(handleWindowResized, 50, false)
+      );
+    },
   };
-}(Drupal));
+})(Drupal);
