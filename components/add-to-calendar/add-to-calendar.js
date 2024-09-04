de2a125714f6c3f99a4b0a19c8ea461a0c0339bf
Christopher Torgalson <manager@bedlamhotel.com>
2024-09-04 09:27:55 +0000

    feat: created add-to-calendar SDC from existing Twig/css/js
    Code unchanged except to make text strings overrideable by calling
    templates.

---
+++ components/add-to-calendar/add-to-calendar.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * JS file for Add to Calendar component.
+ */
+
+(function localgovAddToCalendarScript(Drupal) {
+  Drupal.behaviors.localgovAddToCalendar = {
+    attach(context) {
+      const addToCalendarButtons = once('all-add-to-calendar-buttons', '.localgov-add-to-calendar__trigger', context);
+      if (addToCalendarButtons.length) {
+        addToCalendarButtons.forEach(button => {
+          const calendarModal = button.nextElementSibling;
+          const calendarModalClose = calendarModal.querySelector('.localgov-add-to-calendar__dialog-close-button');
+          button.addEventListener('click', () => {
+            calendarModal.showModal();
+          });
+          calendarModalClose.addEventListener('click', () => {
+            calendarModal.close();
+          });
+        });
+      }
+    }
+  };
+}(Drupal));
