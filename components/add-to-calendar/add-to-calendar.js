f6190cfb9efcc4a80afd23a6ec2236c87e58f895
Mark Conroy <mark@mark.ie>
2024-12-18 12:27:36 +0000

    fixes eslint issues in add-to-calendar.js

--- components/add-to-calendar/add-to-calendar.js
+++ components/add-to-calendar/add-to-calendar.js
@@ -6,19 +6,25 @@
 (function localgovAddToCalendarScript(Drupal) {
   Drupal.behaviors.localgovAddToCalendar = {
     attach(context) {
-      const addToCalendarButtons = once('all-add-to-calendar-buttons', '.localgov-add-to-calendar__trigger', context);
+      const addToCalendarButtons = once(
+        "all-add-to-calendar-buttons",
+        ".localgov-add-to-calendar__trigger",
+        context,
+      );
       if (addToCalendarButtons.length) {
-        addToCalendarButtons.forEach(button => {
+        addToCalendarButtons.forEach((button) => {
           const calendarModal = button.nextElementSibling;
-          const calendarModalClose = calendarModal.querySelector('.localgov-add-to-calendar__dialog-close-button');
-          button.addEventListener('click', () => {
+          const calendarModalClose = calendarModal.querySelector(
+            ".localgov-add-to-calendar__dialog-close-button",
+          );
+          button.addEventListener("click", () => {
             calendarModal.showModal();
           });
-          calendarModalClose.addEventListener('click', () => {
+          calendarModalClose.addEventListener("click", () => {
             calendarModal.close();
           });
         });
       }
-    }
+    },
   };
-}(Drupal));
+})(Drupal);
